20be1f17a9a0ffe9d03f04e9aa9f8b7f200d6ba7
JonMada <jonmadariaga93@gmail.com>
2024-09-24 18:38:32 +0200

    Token update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, {Component} from "react";
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import {jwtDecode} from 'jwt-decode'; 
 import Modal from 'react-modal';
 
 import Home from './components/pages/home';
@@ -9,15 +10,14 @@ import Register from './components/auth/register';
 import MyLibrary from "./components/pages/my-library";
 import BookDetails from "./components/books/book-details";
 import NavBar from "./components/navigation/nav-bar";
-import PrivateRoute from "./components/routes/private-routes";
+
 
 import Icons from '../src/helpers/icons';
 
 Modal.setAppElement('#root');
 
-
- export default class App extends Component {
-  constructor(props){
+export default class App extends Component {
+  constructor(props) {
     super(props);
 
     this.handleAuthChange = this.handleAuthChange.bind(this);
@@ -28,8 +28,30 @@ Modal.setAppElement('#root');
     };
 
     Icons();
-
   }
+
+  componentDidMount() {
+    this.checkTokenExpiration(); 
+  }
+
+  
+  checkTokenExpiration = () => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      try {
+        const decodedToken = jwtDecode(token);
+        const currentTime = Date.now() / 1000; 
+
+        
+        if (decodedToken.exp < currentTime) {
+          this.handleSignOut(); 
+        }
+      } catch (error) {
+        console.error('Error decoding token:', error);
+        this.handleSignOut(); 
+      }
+    }
+  };
 
   handleAuthChange = (status) => {
     this.setState({ isAuthenticated: status });
@@ -40,50 +62,49 @@ Modal.setAppElement('#root');
     this.setState({ isAuthenticated: false });
   };
 
-
-  
-  render(){
-    return(
-      <Router> 
+  render() {
+    return (
+      <Router>
         <div>
-          <NavBar 
+          <NavBar
             isAuthenticated={this.state.isAuthenticated}
             handleSignOut={this.handleSignOut}
           />
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/books" element={<Books />} />
-              <Route path="/register" element={<Register />} />
-              
-              <Route
-                path="/auth"
-                element={
-                  <Login setAuth={this.handleAuthChange} />
-                }
-              />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/books" element={<Books />} />
+            <Route path="/register" element={<Register />} />
+            
+            <Route
+              path="/auth"
+              element={<Login setAuth={this.handleAuthChange} />}
+            />
 
-              <Route
-                path="/my-library"
-                element={
-                  <PrivateRoute isAuthenticated={this.state.isAuthenticated}>
-                    <MyLibrary />
-                  </PrivateRoute>
-                }
-              />
+            
+            <Route
+              path="/my-library"
+              element={
+                this.state.isAuthenticated ? (
+                  <MyLibrary />
+                ) : (
+                  <Navigate to="/auth" />
+                )
+              }
+            />
 
-              <Route
-                path="/book/:slug"
-                element={
-                  <PrivateRoute isAuthenticated={this.state.isAuthenticated}>
-                    <BookDetails />
-                  </PrivateRoute>
-                }
-              />
-
+            <Route
+              path="/book/:slug"
+              element={
+                this.state.isAuthenticated ? (
+                  <BookDetails />
+                ) : (
+                  <Navigate to="/auth" />
+                )
+              }
+            />
           </Routes>
         </div>
-    </Router>
-    )
+      </Router>
+    );
   }
 }
-
